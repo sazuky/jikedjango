0d6ad80076d19c5acd26b755b48b60f2d2106e35
sazuky <412453767@qq.com>
2021-11-16 14:54:45 +0800

    apps
    apps

--- myjk/apps.py
+++ myjk/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class MyjkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myjk'
-    verbose_name = '东哥的博客'
+    verbose_name = '东哥的博客1'
