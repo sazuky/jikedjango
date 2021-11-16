7845a410b5900493e820b1e28b6a0747751bf8ca
sazuky <412453767@qq.com>
2021-11-16 14:51:20 +0800

    Initial commit

---
+++ jkvue/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld v-bind:msg=message />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  data(){
+    return{
+      message:'三星大眼睛'
+    }
+  },
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ myjk/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MyjkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myjk'
+    verbose_name = '东哥的博客'
